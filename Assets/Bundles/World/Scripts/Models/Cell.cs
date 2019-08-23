d3052c32c82b264a05da8bb3c7ba3361cafbb0cb
Donovan <donovan.persent@gmail.com>
2019-08-23 23:12:04 +0200

    Organize folders and create World main system

---
+++ Assets/Bundles/World/Scripts/Models/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldBuilder
+{
+	public struct Cell
+	{
+        public Vector2 position;
+
+        public float? height;
+
+        public Cell(Vector2 position) {
+            this.position = position;
+            height = null;
+        }
+
+        public Cell(Vector2 position, float height):this(position)
+        {
+            this.height = height;
+        }
+    }
+}
---
+++ Assets/Bundles/World/Scripts/Models/WorldObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WorldBuilder.World;
+
+namespace WorldBuilder
+{
+    /// <summary>
+    /// The main class for every scene objects
+    /// </summary>
+	public abstract class WorldObject : MonoBehaviour
+	{
+        #region Properties
+        public Cell currentCell => WorldGrid.GetCellFromWorldObject(this);
+        #endregion
+    }
+}
